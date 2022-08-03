67068661bbea100e10259e6a15fe259cc892aef8
Adi Nugroho <ijlik.kl5s@gmail.com>
2022-08-03 21:56:50 +0700

    first commit

---
+++ database/seeders/SettingTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class SettingTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('setting')->insert([
+            'id_setting' => 1,
+            'nama_perusahaan' => 'Toko Jujur dan Adil',
+            'alamat' => 'Jl. Pancursari Rt 3 RW 1 Desa Benculuk Kecamatan Cluring',
+            'telepon' => '085155187441',
+            'tipe_nota' => 1, // kecil
+            'diskon' => 5,
+            'path_logo' => '/img/logo.png',
+            'path_kartu_member' => '/img/member.png',
+        ]);
+    }
+}
